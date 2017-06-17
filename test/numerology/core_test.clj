d49537a0c96b10546da0911e62d72780d9ca32e4
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-06-17 13:12:59 +0200

    add additional test for the case where there is a 0 to the left on the 2

--- test/numerology/core_test.clj
+++ test/numerology/core_test.clj
@@ -8,4 +8,5 @@
     (is (= [0 10 10 0] (process [0 9 0]))))
 
   (testing "replaces 2 with an equal amount of 1s as the number to the left"
-    (is (= [1 1] (process [1 2]) ))))
+    (is (= [1 1] (process [1 2])))
+    (is (= [0] (process [0 2])))))
