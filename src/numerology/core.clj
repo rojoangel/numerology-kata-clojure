fdd33ab6f7330374bb18632d4abf0bf9e2e52b3d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-06-17 13:52:15 +0200

    refactor to replace case with multi-method

--- src/numerology/core.clj
+++ src/numerology/core.clj
@@ -1,15 +1,21 @@
 (ns numerology.core)
 
-(defn replace-idx [idx ns]
-  (let [n (nth ns idx)]
-    (case n
-      9 [10 10]
-      2 (let [left (nth ns (dec idx))]
-          (repeat left 1))
-      6 (let [left (nth ns (dec idx))
-              repetition (nth ns (+ idx left))]
-          (repeat repetition 3))
-      n)))
+(defmulti replace-idx (fn [idx ns] (nth ns idx)))
+
+(defmethod replace-idx 9 [idx ns]
+  [10 10])
+
+(defmethod replace-idx 2 [idx ns]
+  (let [left (nth ns (dec idx))]
+    (repeat left 1)))
+
+(defmethod replace-idx 6 [idx ns]
+  (let [left (nth ns (dec idx))
+        repetition (nth ns (+ idx left))]
+    (repeat repetition 3)))
+
+(defmethod replace-idx :default [idx ns]
+  (nth ns idx))
 
 (defn process [ns]
   (flatten (map-indexed replace-idx (repeat (count ns) ns))))
