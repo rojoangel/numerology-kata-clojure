8928567649673150f62bf62d398c3d4b98fcf395
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-06-27 21:09:23 +0200

    reorganize tests

--- test/numerology/core_test.clj
+++ test/numerology/core_test.clj
@@ -16,16 +16,14 @@
     (is (= [1 3 1] (process [1 6 1])))
     (is (= [0 3 3 3 3 3 3] (process [0 6]))))
 
-  (testing "replace any 3 by a 5 if not immediately succeeded by 5 and any 4 by a 3 if not immediately preceeded by 5.
-  When replacing the 3's and 4's, you may not replace more than one 3 or 4 in a go without having replaced one instance
-  of the other in between.
-  When four 3's have been replaced, and three 4's have been replaced no more replacements may occur until a 7 is seen.
-  Once a 7 is seen, the whole process is reset and four 3's and three 4's may be replaced again"
-    (is (= [5] (process [3])))
-    (is (= [3 5] (process [3 5])))
-    (is (= [5 5] (process [5 3])))
-    (is (= [0 5] (process [0 3])))
-    (is (= [3] (process [4])))
-    (is (= [5 4] (process [5 4])))
-    (is (= [3 5] (process [4 5])))
-    (is (= [0 3] (process [0 4])))))
+  (testing
+    (testing "replace any 3 by a 5 if not immediately succeeded by 5"
+      (is (= [5] (process [3])))
+      (is (= [3 5] (process [3 5])))
+      (is (= [5 5] (process [5 3])))
+      (is (= [0 5] (process [0 3]))))
+    (testing "and any 4 by a 3 if not immediately preceeded by 5."
+      (is (= [3] (process [4])))
+      (is (= [5 4] (process [5 4])))
+      (is (= [3 5] (process [4 5])))
+      (is (= [0 3] (process [0 4]))))))
