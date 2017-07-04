0a118e9303591c8c49495ba4fd3eaa45eff49231
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-04 23:18:12 +0200

    refactor: change param order to get rid of update-acc intermediate function

--- src/numerology/core.clj
+++ src/numerology/core.clj
@@ -1,20 +1,20 @@
 (ns numerology.core)
 
-(defmulti replace-idx (fn [idx ns _] (nth ns idx)))
+(defmulti replace-idx (fn [_ idx ns] (nth ns idx)))
 
-(defmethod replace-idx 9 [idx ns acc]
+(defmethod replace-idx 9 [acc idx ns]
   (update acc :out (partial apply conj) [10 10]))
 
-(defmethod replace-idx 2 [idx ns acc]
+(defmethod replace-idx 2 [acc idx ns]
   (let [left (nth ns (dec idx))]
     (update acc :out (partial apply conj) (repeat left 1))))
 
-(defmethod replace-idx 6 [idx ns acc]
+(defmethod replace-idx 6 [acc idx ns]
   (let [left (nth ns (dec idx))
         repetition (nth ns (+ idx left))]
     (update acc :out (partial apply conj) (repeat repetition 3))))
 
-(defmethod replace-idx 3 [idx ns {:keys [out threes-n-fours] :as acc}]
+(defmethod replace-idx 3 [{:keys [out threes-n-fours] :as acc} idx ns]
   (let [right (get ns (inc idx))]
     (if (or (= (last threes-n-fours) 3) (= right 5))
       (update acc :out conj 3)
@@ -22,7 +22,7 @@
           (update :out conj 5)
           (update :threes-n-fours conj 3)))))
 
-(defmethod replace-idx 4 [idx ns {:keys [out threes-n-fours] :as acc}]
+(defmethod replace-idx 4 [{:keys [out threes-n-fours] :as acc} idx ns]
   (let [left (get ns (dec idx))]
     (if (or (= (last threes-n-fours) 4)
             (= left 5)
@@ -33,16 +33,16 @@
           (update :out conj 3)
           (update :threes-n-fours conj 4)))))
 
-(defmethod replace-idx 7 [idx ns acc]
+(defmethod replace-idx 7 [acc idx ns]
   (-> acc
       (update :out conj (nth ns idx))
       (assoc :threes-n-fours [])))
 
-(defmethod replace-idx :default [idx ns acc]
+(defmethod replace-idx :default [acc idx ns]
   (update acc :out conj (nth ns idx)))
 
 (defn update-acc [acc [idx ns]]
-  (replace-idx idx ns acc))
+  (replace-idx acc idx ns))
 
 (defn process [ns]
   (:out
