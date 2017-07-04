afc11dd54e6fa82aa086d7bbe7568341e2a70a9f
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-04 22:49:11 +0200

    refactor all to keep track of 3s and 4s instead of replacement counts

--- src/numerology/core.clj
+++ src/numerology/core.clj
@@ -2,49 +2,45 @@
 
 (defmulti replace-idx (fn [idx ns _] (nth ns idx)))
 
-(defmethod replace-idx 9 [idx ns _]
-  {:replacement [10 10] :count-diff 0})
+(defmethod replace-idx 9 [idx ns acc]
+  (update acc :out (partial apply conj) [10 10]))
 
-(defmethod replace-idx 2 [idx ns _]
+(defmethod replace-idx 2 [idx ns acc]
   (let [left (nth ns (dec idx))]
-    {:replacement (repeat left 1) :count-diff 0}))
+    (update acc :out (partial apply conj) (repeat left 1))))
 
-(defmethod replace-idx 6 [idx ns _]
+(defmethod replace-idx 6 [idx ns acc]
   (let [left (nth ns (dec idx))
         repetition (nth ns (+ idx left))]
-    {:replacement (repeat repetition 3) :count-diff 0}))
+    (update acc :out (partial apply conj) (repeat repetition 3))))
 
-(defmethod replace-idx 3 [idx ns replacement-counts]
-  (if (even? replacement-counts)
-    (if (> (dec (count ns)) idx)
-      (let [right (nth ns (inc idx))]
-        (if (= right 5)
-          {:replacement 3 :count-diff 0}
-          {:replacement 5 :count-diff 1}))
-      {:replacement 5 :count-diff 0})
-    {:replacement 3 :count-diff 0}))
+(defmethod replace-idx 3 [idx ns {:keys [out threes-n-fours] :as acc}]
+  (let [right (get ns (inc idx))]
+    (if (or (= (last threes-n-fours) 3) (= right 5))
+      (update acc :out conj 3)
+      (-> acc
+          (update :out conj 5)
+          (update :threes-n-fours conj 3)))))
 
-(defmethod replace-idx 4 [idx ns _]
-  (if (> idx 0)
-    (let [left (nth ns (dec idx))]
-      (if (= left 5)
-        {:replacement 4 :count-diff 0}
-        {:replacement 3 :count-diff 0}))
-    {:replacement 3 :count-diff 0}))
+(defmethod replace-idx 4 [idx ns {:keys [out threes-n-fours] :as acc}]
+  (let [left (get ns (dec idx))]
+    (if (or (= (last threes-n-fours) 4)
+            (= left 5))
+      (update acc :out conj 4)
+      (-> acc
+          (update :out conj 3)
+          (update :threes-n-fours conj 4)))))
 
-(defmethod replace-idx :default [idx ns _]
-  {:replacement (nth ns idx) :count-diff 0})
+(defmethod replace-idx :default [idx ns acc]
+  (update acc :out conj (nth ns idx)))
 
 (defn update-acc [acc [idx ns]]
-  (let [replacement-result (replace-idx idx ns (:replacement-counts acc))]
-    (update-in
-      (update-in acc [:out] conj (:replacement replacement-result))
-      [:replacement-counts] + (:count-diff replacement-result))))
+  (replace-idx idx ns acc))
 
 (defn process [ns]
   (flatten
     (:out
       (reduce
         update-acc
-        {:replacement-counts 0 :out []}
+        {:threes-n-fours [] :out []}
         (map-indexed vector (repeat (count ns) ns))))))
